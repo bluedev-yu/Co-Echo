0b19a682d16ba5ee4f5d0480b0a679361fca91dc
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-07-29 05:56:00 +0900

    fragment added

--- app/src/main/java/bluedev_yu/coecho/MainActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MainActivity.kt
@@ -2,10 +2,17 @@ package bluedev_yu.coecho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    public fun switchFragment(){
+        // 프래그먼트 교체 함수
     }
 }
