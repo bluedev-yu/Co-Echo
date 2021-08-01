4e37bd1a55913c7af7e4d58752634bb6e7c55778
Hyeju KIL <mmow110@naver.com>
2021-08-01 22:37:19 +0900

    db연동
      Firebase 연동 및 Firebase, Firestorage객체 생성

--- app/src/main/java/bluedev_yu/coecho/MainActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MainActivity.kt
@@ -2,10 +2,20 @@ package bluedev_yu.coecho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 
 class MainActivity : AppCompatActivity() {
+
+    var firestore : FirebaseFirestore?= null //String 등 자료형 데이터베이스
+    var firestorage : FirebaseStorage?= null //사진, GIF 등의 파일 데이터베이스
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firestore = FirebaseFirestore.getInstance() //객체 할당
+        firestorage = FirebaseStorage.getInstance()
+
     }
 }
