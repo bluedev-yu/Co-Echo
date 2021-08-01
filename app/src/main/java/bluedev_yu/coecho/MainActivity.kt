ae097da0a899ba6b22886c66e891bf6a6006dfec
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-08-01 13:48:15 +0900

    Fragment transition added, Set Default Fragment

--- app/src/main/java/bluedev_yu/coecho/MainActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MainActivity.kt
@@ -2,17 +2,51 @@ package bluedev_yu.coecho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import bluedev_yu.coecho.Fragment.FragmentMap
+import bluedev_yu.coecho.Fragment.FragmentMyPage
+import bluedev_yu.coecho.Fragment.FragmentSNS
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        bottomNavBar.setOnItemSelectedListener(this)
 
+        setDefaultFragment()
     }
 
-    public fun switchFragment(){
-        // 프래그먼트 교체 함수
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sns -> {
+                loadFragment(FragmentSNS())
+                return true
+            }
+            R.id.action_map -> {
+                loadFragment(FragmentMap())
+                return true
+            }
+            R.id.action_myPage -> {
+                loadFragment(FragmentMyPage())
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun setDefaultFragment(){
+        loadFragment(FragmentSNS())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
     }
 }
