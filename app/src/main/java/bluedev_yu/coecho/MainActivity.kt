89bea06258cc31d451055122689e556e50d52ee5
yeaaaaahhhhh <72369871+yeaaaaahhhhh@users.noreply.github.com>
2021-08-01 18:10:02 +0900

    지도 api 보이기만 적용 하지만 버전 문제로 테스트 되지 못함 사용자 위치추적 권한 요청 기능 추가

--- app/src/main/java/bluedev_yu/coecho/MainActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MainActivity.kt
@@ -1,11 +1,37 @@
 package bluedev_yu.coecho
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import net.daum.mf.map.api.MapView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.mapmain)
+        val mapView = MapView(this)
+        //mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+        val mapViewContainer = findViewById<View>(R.id.map_view) as ViewGroup
+        mapViewContainer.addView(mapView)
+        if(PackageManager.PERMISSION_DENIED==ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION))
+        {
+            System.out.println("***denied***")
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1004)//requestcode 1004로 해도 되는지 확인 필요
+            if(PackageManager.PERMISSION_DENIED==ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION))
+                System.out.println("***still denied***")
+        }
+        else if(PackageManager.PERMISSION_GRANTED==ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION))
+            System.out.println("***granted***")
     }
+
+
 }
