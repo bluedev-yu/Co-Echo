e1ee403dcf71704607ce2f546f125c4f1a8a8f6f
Hyeju KIL <mmow110@naver.com>
2021-08-10 21:15:28 +0900

    create custom marker

--- app/src/main/java/bluedev_yu/coecho/MainActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MainActivity.kt
@@ -17,8 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //--해시 키 발급--
-        /*var keyHash = Utility.getKeyHash(this)
-        Log.d("해시 키",keyHash)*/
+//        var keyHash = Utility.getKeyHash(this)
+//        Log.d("해시 키",keyHash)
         val nextIntent = Intent(this, MapActivity::class.java)
         startActivity(nextIntent)
     }
--- app/src/main/java/bluedev_yu/coecho/MapActivity.kt
+++ app/src/main/java/bluedev_yu/coecho/MapActivity.kt
@@ -1,6 +1,8 @@
 package bluedev_yu.coecho
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
@@ -9,24 +11,33 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.FragmentManager
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 class MapActivity : AppCompatActivity() {
+
+    val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mapmain)
+
+
         val mapView = MapView(this)
         val mapViewContainer = findViewById<View>(R.id.map_view) as ViewGroup
         mapViewContainer.addView(mapView)
 
         //마커 추가 코드
-        mapView.addPOIItem(newMapPoiItem("제로스테이",35.81943491247418,128.52228528837313))
+        mapView.addPOIItem(newCustomMapPoiItem("제로스테이",35.81943491247418,128.52228528837313))
+        mapView.addPOIItem(newCustomMapPoiItem("더 커먼",35.86863293890943, 128.61172204038763))
 
         //맵 초기 위치 설정(마커 있는 진천동 부근으로 임의설정)
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.81, 128.52), true);
@@ -63,6 +74,7 @@ class MapActivity : AppCompatActivity() {
 
         //맵뷰에 이벤트 리스너 적용
         mapView.setMapViewEventListener(mapltner)
+
     }
     val mapltner = object : MapView.MapViewEventListener {
         override fun onMapViewInitialized(mapView: MapView) {
@@ -102,6 +114,8 @@ class MapActivity : AppCompatActivity() {
         }
     }
 
+
+
     fun newMapPoiItem(iName:String,lat:Double,lon:Double):MapPOIItem//지도에 마커 추가 함수
     {
         val marker = MapPOIItem()
@@ -110,9 +124,58 @@ class MapActivity : AppCompatActivity() {
             mapPoint = MapPoint.mapPointWithGeoCoord(lat, lon)
             markerType = MapPOIItem.MarkerType.BluePin
             selectedMarkerType = MapPOIItem.MarkerType.BluePin
+            isCustomImageAutoscale = true
+            setCustomImageAnchor(0.5f, 1.0f)
+        }
+        return marker
+    }
+
+
+    fun newCustomMapPoiItem(iName:String, lat: Double, lon: Double):MapPOIItem{
+        val marker = MapPOIItem();
+        marker.apply {
+            itemName = iName
+            tag = 1
+            mapPoint = MapPoint.mapPointWithGeoCoord(lat, lon)
+            markerType = MapPOIItem.MarkerType.CustomImage // 마커타입을 커스텀 마커로 지정.
+            customImageResourceId = R.drawable.pin //마커 이미지 설정 -> 나뭇잎 모양
             isCustomImageAutoscale = false
             setCustomImageAnchor(0.5f, 1.0f)
         }
         return marker
     }
+
+
+
+    val listener = object : MapView.POIItemEventListener{
+        override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+            //마커 클릭 이벤트
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {
+            //말품선 클릭 1
+            TODO("Not yet implemented")
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(
+            p0: MapView?,
+            p1: MapPOIItem?,
+            p2: MapPOIItem.CalloutBalloonButtonType?
+            //말풍선 클릭 2
+        ) {
+
+//            supportFragmentManager.beginTransaction()
+//                .add(R.id.framelayout, Place_detail())
+//                .commit()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
+            //마커 속성이 idDraggable = true일 때 마커 이동시
+        }
+    }
+
 }
