db24801bda311a94e5a6f6a4d0f28360183858cb
Hyeju KIL <mmow110@naver.com>
2021-09-14 14:55:29 +0900

    splash database->string.xml
    속도 빨라짐, 디자인 개선 필요

--- app/src/main/java/bluedev_yu/coecho/splash.kt
+++ app/src/main/java/bluedev_yu/coecho/splash.kt
@@ -1,36 +1,38 @@
 package bluedev_yu.coecho
 
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import bluedev_yu.coecho.Model.SplashDTO
+import bluedev_yu.coecho.databinding.ActivitySplashBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
 
 class splash : AppCompatActivity() {
+    private var mBinding: ActivitySplashBinding? = null
+
+    private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        mBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         var db = Firebase.firestore
 
         val random = Random()
         var num = random.nextInt(6)
 
-        db.collection("Splash")?.document(num.toString())?.get().addOnCompleteListener{
-            task->
-            if(task.isSuccessful)
-            {
-                var splashDTO = task.result?.toObject(SplashDTO::class.java)
-                if (splashDTO != null) {
-                    spashText.setText(splashDTO.text)
-                }
-            }
-        }
+        val res : Resources = resources
+        var splashArray = res.getStringArray(R.array.splash)
+
+        var text = splashArray[num].toString()
+
+        binding.splashText.setText(text)
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
@@ -41,7 +43,7 @@ class splash : AppCompatActivity() {
     }
 
     companion object{
-        private const val DURATION : Long = 5000
+        private const val DURATION : Long = 3000
     }
 
     override fun onBackPressed() {
