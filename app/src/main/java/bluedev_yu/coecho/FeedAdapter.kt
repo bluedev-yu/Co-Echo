89662cfc7502696e47d7d46166dec5dc3b9c606f
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-09-26 16:20:39 +0900

    recyclerview text added
    피드 : 프로필 + 텍스트

--- app/src/main/java/bluedev_yu/coecho/FeedAdapter.kt
+++ app/src/main/java/bluedev_yu/coecho/FeedAdapter.kt
@@ -17,6 +17,7 @@ class FeedAdapter(val feedList: ArrayList<Feeds>) : RecyclerView.Adapter<FeedAda
     override fun onBindViewHolder(holder: FeedAdapter.CustomViewHolder, position: Int) {
         holder.gender.setImageResource(feedList.get(position).gender)
         holder.name.text = feedList.get(position).name
+        holder.text.text = feedList.get(position).text
     }
 
     override fun getItemCount(): Int {
@@ -26,7 +27,7 @@ class FeedAdapter(val feedList: ArrayList<Feeds>) : RecyclerView.Adapter<FeedAda
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val gender = itemView.findViewById<ImageView>(R.id.iv_profile) //성별
         val name = itemView.findViewById<TextView>(R.id.tv_name) //이름
-
+        val text = itemView.findViewById<TextView>(R.id.tv_text) //피드 글
     }
 
 }
