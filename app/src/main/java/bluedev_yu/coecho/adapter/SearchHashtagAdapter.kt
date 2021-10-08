1d26e449322bb854d41e3ae72cb0a43c9539b954
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-10-08 21:27:46 +0900

    search hashtag result recyclerview added

---
+++ app/src/main/java/bluedev_yu/coecho/adapter/SearchHashtagAdapter.kt
@@ -0,0 +1,32 @@
+package bluedev_yu.coecho.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import bluedev_yu.coecho.R
+import bluedev_yu.coecho.data.model.Feeds
+
+class SearchHashtagAdapter(val hashtagList: ArrayList<Feeds>): RecyclerView.Adapter<SearchHashtagAdapter.CustomViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_search_hashtag, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.hashtag.text = hashtagList.get(position).hashtag
+    }
+
+
+    override fun getItemCount(): Int {
+        return hashtagList.size
+    }
+
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var hashtag = itemView.findViewById<TextView>(R.id.tv_search_hashtag) //해시태그
+    }
+
+
+}
