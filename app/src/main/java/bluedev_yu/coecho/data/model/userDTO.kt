2cc6c30a43fe06bd416cfd5579d315293018b560
Hyeju KIL <mmow110@naver.com>
2021-10-31 18:15:46 +0900

    imageupload making

--- app/src/main/java/bluedev_yu/coecho/data/model/userDTO.kt
+++ app/src/main/java/bluedev_yu/coecho/data/model/userDTO.kt
@@ -5,4 +5,6 @@ import java.net.URL
 data class userDTO(var strName : String? = null,//사용자 이름
                     var uid : String? = null,//uid
                    var imageUrl : String?= null, //프로필사진
-                   var title : Int?= 0 ) //칭호
+                   var title : Int?= 0 ,//칭호
+                    var mypageBackground : String?= null
+) //마이페이지 뒷배경
