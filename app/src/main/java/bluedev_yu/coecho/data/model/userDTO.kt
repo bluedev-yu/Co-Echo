c16086a4395e8fa204f99e0315d410fdb8d09b51
Hyeju KIL <mmow110@naver.com>
2021-11-02 10:32:42 +0900

    bookmark added

--- app/src/main/java/bluedev_yu/coecho/data/model/userDTO.kt
+++ app/src/main/java/bluedev_yu/coecho/data/model/userDTO.kt
@@ -6,5 +6,6 @@ data class userDTO(var strName : String? = null,//사용자 이름
                     var uid : String? = null,//uid
                    var imageUrl : String?= null, //프로필사진
                    var title : Int?= 0 ,//칭호
-                    var mypageBackground : String?= null
+                    var mypageBackground : String?= null,
+                    var placeLike : MutableMap<String, String> = mutableMapOf()
 ) //마이페이지 뒷배경
--- app/src/main/java/bluedev_yu/coecho/fragment/PlaceDetailFragment.kt
+++ app/src/main/java/bluedev_yu/coecho/fragment/PlaceDetailFragment.kt
@@ -14,9 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import bluedev_yu.coecho.*
 import bluedev_yu.coecho.adapter.ReviewAdapter
+import bluedev_yu.coecho.data.model.Feeds
 import bluedev_yu.coecho.data.model.Place
 import bluedev_yu.coecho.data.model.ReviewDTO
+import bluedev_yu.coecho.data.model.userDTO
 import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_place_detail.*
@@ -26,6 +30,9 @@ import kotlinx.coroutines.tasks.await
 
 class PlaceDetailFragment : Fragment() {
 
+    var firebase : FirebaseFirestore?= null
+    var auth : FirebaseAuth ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -34,6 +41,10 @@ class PlaceDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        firebase = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
         var reviewList = arrayListOf<ReviewDTO>()
         var thisFragView = inflater.inflate(R.layout.fragment_place_detail, container, false)
         val pName = arguments?.getString("name")
@@ -50,12 +61,7 @@ class PlaceDetailFragment : Fragment() {
         thisFragView.tv_phone_placedetail.setText(pPhone)
         thisFragView.tv_url_placedetail.setText(pUrl)
 
-        thisFragView.ib_backbutton.setOnClickListener {
-
-        }
-
-
-        lateinit var pid: String
+        var pid: String? = null
         val t_rv_review = thisFragView.findViewById<RecyclerView>(R.id.rv_placeReview)
         t_rv_review.layoutManager =
             LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
@@ -74,7 +80,7 @@ class PlaceDetailFragment : Fragment() {
                 Toast.makeText(activity, "리뷰가 아직 없습니다", Toast.LENGTH_LONG).show()
             } else {
                 CoroutineScope(Dispatchers.Main).launch {
-                    tempRes = DB_Review().getHashtag(pid)
+                    tempRes = DB_Review().getHashtag(pid!!)
                     if (tempRes != null) {
                         if (tempRes!!.first != "") {
                             layout_hashtag1.visibility = View.VISIBLE
@@ -88,7 +94,7 @@ class PlaceDetailFragment : Fragment() {
                         layout_hashtag1.visibility = View.INVISIBLE
                         layout_hashtag2.visibility = View.INVISIBLE
                     }
-                    loadPlaceReview(pid, reviewList)
+                    loadPlaceReview(pid!!, reviewList)
                     Log.i("로드되었다", reviewList.toString())
                     val tvCount = thisFragView.findViewById<TextView>(R.id.tv_review_cnt)
                     tvCount.setText("방문자리뷰 " + reviewList.size)
@@ -98,6 +104,32 @@ class PlaceDetailFragment : Fragment() {
             }
         }
 
+        thisFragView.ib_backbutton.setOnClickListener {
+        }
+
+        firebase?.collection("User")?.document(auth?.uid.toString())?.addSnapshotListener{
+                documentSnapshot, FirebaseFirestoreException ->
+            //미리 북마크가 비었는가 찼는가
+            var doc = documentSnapshot?.toObject(userDTO::class.java)
+            CoroutineScope(Dispatchers.Main).launch {
+                pid = DB_Place().search_data(
+                    pName!!, pAddress!!
+                )
+            Log.v("bookmark",pid.toString())
+            if (doc?.placeLike!!.containsKey(pid)) //좋아요 눌렀을 경우
+            {
+                Log.v("bookmark","filled")
+                thisFragView.bookmark.setBackgroundResource(R.drawable.bookmark_filled)
+            } else {
+                Log.v("bookmark","empty")
+                thisFragView.bookmark.setBackgroundResource(R.drawable.bookmark_empty)
+            }
+            }
+        }
+
+        thisFragView.bookmark.setOnClickListener{
+            placelikeEvent(pid!!)
+        }
 
 
         thisFragView.findViewById<Button>(R.id.btn_write_review).setOnClickListener {
@@ -143,6 +175,7 @@ class PlaceDetailFragment : Fragment() {
             }
         }
 
+
         // Inflate the layout for this fragment
         return thisFragView
     }
@@ -166,5 +199,26 @@ class PlaceDetailFragment : Fragment() {
         }
     }
 
+    fun placelikeEvent(pid : String)
+    {
+        var tsDoc = firebase?.collection("User")?.document(auth?.uid.toString())
+        firebase?.runTransaction{
+                transaction ->
+
+            val uid = auth?.uid.toString()
+            val userDTO = transaction.get(tsDoc!!).toObject(userDTO::class.java)
+
+            if(userDTO!!.placeLike.containsKey(pid)){ //이미 북마크 한 경우 -> 북마크 철회
+                Log.v("bookmark","철회")
+                userDTO?.placeLike.remove(pid)
+            }
+            else //좋아요 아직 안함 -> 좋아요 하기
+            {
+                Log.v("bookmark","추가")
+                userDTO.placeLike[pid] = pid
+            }
+            transaction.set(tsDoc,userDTO)
+        }
+    }
 
 }
